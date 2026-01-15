1885d7efaed771ebeb98ffbd6e8bba48288abe4a
rahul-debugs <sarathrahul080@gmail.com>
2026-01-15 10:27:16 +0530

    the update of watchlist

--- frontend/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Auth from './auth'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Auth />
-  </React.StrictMode>,
-)
